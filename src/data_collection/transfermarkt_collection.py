a74ad698b3dded0afabd70edb7053a144372bc40
jakep <jakeparperis@gmail.com>
2025-07-27 21:20:50 +0300

    Add functionality to go to detailed view and refactor iterate_seasons_pages() to specify the transfer window - select_transfer_window() not yet implemented

--- src/data_collection/transfermarkt_collection.py
+++ src/data_collection/transfermarkt_collection.py
@@ -1,10 +1,8 @@
-from scipy.stats import tmean
-
-from selenium_config import driver, By, EC
+from selenium_config import driver, By
 import time
 import re
 
-base_url = "https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/saison_id/{season}/transferfenster/alle/land_id//ausrichtung//spielerposition_id//altersklasse//leihe//plus/0/galerie/0/page/{page}"
+base_url = "https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/saison_id/{season}/transferfenster/alle/land_id//ausrichtung//spielerposition_id//altersklasse//leihe//plus/1/galerie/0/page/{page}"
 pagination_xpath = "//*[@id='yw0']/div[2]/ul/li[12]/a"
 pagination_CSS = 'a.tm-pagination__link[title*="Go to the last page"]'
 
@@ -21,19 +19,27 @@ def scrape_pages_num():
             return 1
     except:
         print("no pages element")
-    
+
+def select_transfer_window(window):
 
 def iterate_seasons_pages(seasons_start, seasons_end):
     for season in range(seasons_start, seasons_end + 1):
         driver.get(base_url.format(season=season, page=1))
         time.sleep(2)
-        pages = scrape_pages_num()
 
+        select_transfer_window("Summer transfers")
+        pages = scrape_pages_num()
         for page in range(1, pages + 1):
             driver.get(base_url.format(season=season, page=page))
             time.sleep(2)
 
-iterate_seasons_pages(2022, 2023)
+        select_transfer_window("Winter transfers")
+        pages = scrape_pages_num()
+        for page in range(1, pages + 1):
+            driver.get(base_url.format(season=season, page=page))
+            time.sleep(2)
+
+iterate_seasons_pages(2015, 2023)
 
 time.sleep(5)
 driver.quit()

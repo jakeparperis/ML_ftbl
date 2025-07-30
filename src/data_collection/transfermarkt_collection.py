039de1072001b570f8dcef203d9c3858fe8073b9
Alexiboi <alexhanna413@gmail.com>
2025-07-30 21:10:11 +0300

    Refactor base_url and update iterate_seasons_pages() to start on summer window page, then move to winter page

--- src/data_collection/transfermarkt_collection.py
+++ src/data_collection/transfermarkt_collection.py
@@ -1,8 +1,10 @@
 from selenium_config import driver, By
 import time
 import re
+from selenium.webdriver.common.keys import Keys
 
-base_url = "https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/saison_id/{season}/transferfenster/alle/land_id//ausrichtung//spielerposition_id//altersklasse//leihe//plus/1/galerie/0/page/{page}"
+
+base_url = "https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id={season}&transferfenster={window}&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&page={page}"
 pagination_xpath = "//*[@id='yw0']/div[2]/ul/li[12]/a"
 pagination_CSS = 'a.tm-pagination__link[title*="Go to the last page"]'
 
@@ -20,23 +22,22 @@ def scrape_pages_num():
     except:
         print("no pages element")
 
-def select_transfer_window(window):
-
+# start on summer transfer page then move onto winter transfers
 def iterate_seasons_pages(seasons_start, seasons_end):
     for season in range(seasons_start, seasons_end + 1):
-        driver.get(base_url.format(season=season, page=1))
+        driver.get(base_url.format(season=season, window="sommertransfers", page=1))
         time.sleep(2)
 
-        select_transfer_window("Summer transfers")
         pages = scrape_pages_num()
         for page in range(1, pages + 1):
-            driver.get(base_url.format(season=season, page=page))
+            driver.get(base_url.format(season=season, window="sommertransfers", page=page))
             time.sleep(2)
 
-        select_transfer_window("Winter transfers")
+        # Get onto page 1 of winter transfers    
+        driver.get(base_url.format(season=season, window = "wintertransfers", page=1))
         pages = scrape_pages_num()
         for page in range(1, pages + 1):
-            driver.get(base_url.format(season=season, page=page))
+            driver.get(base_url.format(season=season, window="wintertransfers", page=page))
             time.sleep(2)
 
 iterate_seasons_pages(2015, 2023)
